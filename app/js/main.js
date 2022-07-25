7d05bf6fa031ae5a073c6e2a9a43661fe15bcc4a
mizza-cz <mizza.cz666@gmail.com>
2022-07-25 09:49:43 +0200

    add fix typo

--- app/js/main.js
+++ app/js/main.js
@@ -1,50 +1,52 @@
 $(function () {
-	$('.js-select').select2({
-		width: '100%',
-		minimumResultsForSearch: -1
-	 });
-  $('.gallery .group').magnificPopup({
+  $('.js-select').select2({
+    width: '100%',
+    minimumResultsForSearch: -1,
+  });
+  $('.gallery .group, .typo__gallery').magnificPopup({
     delegate: 'a',
     type: 'image',
     gallery: {
       enabled: true,
     },
-  })
-   $('.navbar__link').on('click', function () {
-      $(this).closest('.navbar__list').toggleClass('active');
-    });
-    $('.navbar__btns').on('click', function () {
-      $('.mobile-wrapper').slideToggle();
-    });
-    $('.nav-slide').slick({
-      infinite: true,
-      autoplay: true,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll:1
-    });
-    var mixer = mixitup('.products');
-})
+  });
+  $('.navbar__link').on('click', function () {
+    $(this).closest('.navbar__list').toggleClass('active');
+  });
+  $('.navbar__btns').on('click', function () {
+    $('.mobile-wrapper').slideToggle();
+  });
+  $('.nav-slide').slick({
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+  var mixer = mixitup('.products');
+});
 let productsInCart = JSON.parse(localStorage.getItem('shoppingCart'));
-if(!productsInCart){
-	productsInCart = [];
+if (!productsInCart) {
+  productsInCart = [];
 }
 const parentElement = document.querySelector('#buyItems');
 const cartSumPrice = document.querySelector('#sum-prices');
 const products = document.querySelectorAll('.mix');
-if(cartSumPrice){
-	const countTheSumPrice = function () { // 4
-		let sum = 0;
-		productsInCart.forEach(item => {
-			sum += item.price;
-		});
-		return sum;
-	}
-	const updateShoppingCartHTML = function () {  // 3
-		localStorage.setItem('shoppingCart', JSON.stringify(productsInCart));
-		if (productsInCart.length > 0) {
-			let result = productsInCart.map(product => {
-				return `
+if (cartSumPrice) {
+  const countTheSumPrice = function () {
+    // 4
+    let sum = 0;
+    productsInCart.forEach((item) => {
+      sum += item.price;
+    });
+    return sum;
+  };
+  const updateShoppingCartHTML = function () {
+    // 3
+    localStorage.setItem('shoppingCart', JSON.stringify(productsInCart));
+    if (productsInCart.length > 0) {
+      let result = productsInCart.map((product) => {
+        return `
 					<div class="buyItem">
 						<h5>${product.name}</h5>
 						<div class="product__right">
@@ -54,96 +56,97 @@ if(cartSumPrice){
 								<input class="countOfProduct" name="id:${product.id}" value="${product.count}">
 								<button class="button-plus" data-id=${product.id}>+</button>
 							</div>
-							${product.size ? `
+							${
+                product.size
+                  ? `
 							<select class="size js-select" name="size:${product.id}">
 								<option value="Kids">Kids</option>
 								<option value="Youth">Youth</option>
 								<option value="Junior"">Junior</option>
-							</select>` : ''}
+							</select>`
+                  : ''
+              }
 						</div>
-					</div>`
-			});
-			parentElement.innerHTML = result.join('');
-			cartSumPrice.innerHTML =  countTheSumPrice() + ' Kč'; 
-		}
-		else {
-			parentElement.innerHTML = ' ';
-		}
-	}
-	function updateProductsInCart(product) { // 2
-		for (let i = 0; i < productsInCart.length; i++) {
-			if (productsInCart[i].id == product.id) {
-				productsInCart[i].count += 1;
-				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-				return;
-			}
-		}
-		productsInCart.push(product);
-	}
-	products.forEach(item => {   // 1
-		item.addEventListener('click', (e) => {
-			if (e.target.classList.contains('addToCart')) {
-				const productSize = e.target.dataset.productSize;
-				const productID = e.target.dataset.productId;
-				const productName = item.querySelector('h3').innerHTML;
-				const productPrice = item.querySelector('.priceValue').innerHTML;
-				const productImage = item.querySelector('img').src;
-				let product = {
-					name: productName,
-					image: productImage,
-					id: productID,
-					count: 1,  
-					size: productSize,
-					price: +productPrice,
-					basePrice: +productPrice,
-				}
-				updateProductsInCart(product);
-				updateShoppingCartHTML();
-			}
-		});
-	});
-	parentElement.addEventListener('click', (e) => { // Last
-		const isPlusButton = e.target.classList.contains('button-plus');
-		const isMinusButton = e.target.classList.contains('button-minus');
-		if (isPlusButton || isMinusButton) {
-			for (let i = 0; i < productsInCart.length; i++) {
-				if (productsInCart[i].id == e.target.dataset.id) {
-					if (isPlusButton) {
-						productsInCart[i].count += 1
-					}
-					else if (isMinusButton) { 
-						productsInCart[i].count -= 1
-					}
-					productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-				}
-				if (productsInCart[i].count <= 0) {
-					productsInCart.splice(i, 1);
-				}
-			}
-			updateShoppingCartHTML();
-		}
-	});
-	updateShoppingCartHTML();
+					</div>`;
+      });
+      parentElement.innerHTML = result.join('');
+      cartSumPrice.innerHTML = countTheSumPrice() + ' Kč';
+    } else {
+      parentElement.innerHTML = ' ';
+    }
+  };
+  function updateProductsInCart(product) {
+    // 2
+    for (let i = 0; i < productsInCart.length; i++) {
+      if (productsInCart[i].id == product.id) {
+        productsInCart[i].count += 1;
+        productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+        return;
+      }
+    }
+    productsInCart.push(product);
+  }
+  products.forEach((item) => {
+    // 1
+    item.addEventListener('click', (e) => {
+      if (e.target.classList.contains('addToCart')) {
+        const productSize = e.target.dataset.productSize;
+        const productID = e.target.dataset.productId;
+        const productName = item.querySelector('h3').innerHTML;
+        const productPrice = item.querySelector('.priceValue').innerHTML;
+        const productImage = item.querySelector('img').src;
+        let product = {
+          name: productName,
+          image: productImage,
+          id: productID,
+          count: 1,
+          size: productSize,
+          price: +productPrice,
+          basePrice: +productPrice,
+        };
+        updateProductsInCart(product);
+        updateShoppingCartHTML();
+      }
+    });
+  });
+  parentElement.addEventListener('click', (e) => {
+    // Last
+    const isPlusButton = e.target.classList.contains('button-plus');
+    const isMinusButton = e.target.classList.contains('button-minus');
+    if (isPlusButton || isMinusButton) {
+      for (let i = 0; i < productsInCart.length; i++) {
+        if (productsInCart[i].id == e.target.dataset.id) {
+          if (isPlusButton) {
+            productsInCart[i].count += 1;
+          } else if (isMinusButton) {
+            productsInCart[i].count -= 1;
+          }
+          productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+        }
+        if (productsInCart[i].count <= 0) {
+          productsInCart.splice(i, 1);
+        }
+      }
+      updateShoppingCartHTML();
+    }
+  });
+  updateShoppingCartHTML();
 }
-
-
 
 function tablesWrap() {
-	var contentTables = document.querySelectorAll(".typo table"),
-      i;
+  var contentTables = document.querySelectorAll('.typo table'),
+    i;
 
-	for (i = 0; i < contentTables.length; ++i) {
-		contentTables[i].classList.add("table");
+  for (i = 0; i < contentTables.length; ++i) {
+    contentTables[i].classList.add('table');
 
-    var contentTableWrap = document.createElement("div");
+    var contentTableWrap = document.createElement('div');
 
-    contentTableWrap.classList.add("table-responsive");
+    contentTableWrap.classList.add('table-responsive');
 
-		contentTables[i].parentNode.insertBefore(contentTableWrap, contentTables[i]);
+    contentTables[i].parentNode.insertBefore(contentTableWrap, contentTables[i]);
 
-		contentTableWrap.appendChild(contentTables[i]);
-	}
+    contentTableWrap.appendChild(contentTables[i]);
+  }
 }
 tablesWrap();
-
-
